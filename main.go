2269705deddf8150d557574405485232d2efcec2
iamcodingabit <irfananandri@gmail.com>
2026-01-03 20:34:20 +0700

    add command, doing loop to add a prexesiting wrestler over and over again to test command

--- main.go
+++ main.go
@@ -4,6 +4,48 @@ import (
 	"fmt"
 )
 
+type Wrestler struct {
+	ringname string
+	nickname string
+	alignment string
+	signature string
+	finisher string
+}
+
+var Commands = map[string]interface{}{
+	"add": addWrestler,
+}
+	
+
+
 func main(){
-	fmt.Println("Bah Gawd, world")
+	wrestlers := []Wrestler{}
+	
+	fmt.Print("Keep adding by typing 'add': ")
+	input := ""
+	isActive := true
+	//Adding wrestlers
+	
+
+	for isActive {
+		fmt.Scan(&input)
+		if input == "add" {
+			Commands[input].(func(*[]Wrestler))(&wrestlers)
+			fmt.Println(wrestlers)
+		} else {
+			isActive = false
+		}
+	}
+	
+	fmt.Println("Thanks for using the app!")
+}
+
+func addWrestler(wrestlers *[]Wrestler){
+	var var1 Wrestler
+	var1.ringname = "Billy Graham"
+	var1.nickname = "Superstar"
+	var1.signature = "Suplex"
+	var1.finisher = "Powerslam"
+	
+	*wrestlers = append(*wrestlers, var1)
 }
