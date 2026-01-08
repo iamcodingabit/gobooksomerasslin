734951d26fa9c3bbf932502d87228d1e8553b345
iamcodingabit <irfananandri@gmail.com>
2026-01-08 21:59:21 +0700

    added listWrestlers

--- main.go
+++ main.go
@@ -17,24 +17,28 @@ type Wrestler struct {
 
 var Commands = map[string]interface{}{
 	"add": addWrestler,
+	"remove": addWrestler,
+	"edit": addWrestler,
+	"finish": addWrestler,
 }
 
 func main(){
 	scanner := bufio.NewScanner(os.Stdin)
 	wrestlers := []Wrestler{}
 	
-	fmt.Print("Add wrestlers by typing 'add', type 'quit' to quit: ")
 	input := ""
 	isActive := true
 	
 	for isActive {
+		fmt.Print("Add wrestlers by typing 'add', type 'quit' to quit: ")
 		scanner.Scan()
+		
 		input = scanner.Text()
 		command := strings.SplitAfterN(input, " ", 2)
 
 		if _, ok := Commands[strings.Trim(command[0], " ")]; ok {
 			Commands[strings.Trim(command[0], " ")].(func(*[]Wrestler, string))(&wrestlers, command[1])
-			fmt.Println(wrestlers)
+			listWrestlers(wrestlers)
 		} else {
 			isActive = false
 		}
@@ -47,4 +51,10 @@ func addWrestler(wrestlers *[]Wrestler, newWrestler string){
 	var var1 Wrestler
 	var1.ringname = newWrestler
 	*wrestlers = append(*wrestlers, var1)
+}
+
+func listWrestlers(wrestlers []Wrestler){
+	for _, v := range wrestlers {
+		fmt.Println(v.ringname)
+	}
 }
