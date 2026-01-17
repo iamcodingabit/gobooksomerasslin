1cda475138a142327f52bf1f835a03002c5457d3
iamcodingabit <notnicekasual@gmail.com>
2026-01-17 10:35:31 +0700

    dunno about this tbh

---
+++ domain/wrestler.go
@@ -0,0 +1,15 @@
+package wrestler
+
+import (
+	//"fmt"
+)
+
+type Wrestler struct {
+	ringname string
+	alignment string // face | heel | tweener?
+	signature_move string
+}
+
+type WrestlerBehavior interface {
+	
+}
