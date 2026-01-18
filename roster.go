b788d2887314c15f146c47eb923abc5416c853c2
iamcodingabit <notnicekasual@gmail.com>
2026-01-18 09:58:49 +0700

    psuedo-coding add and list wrestlers

--- roster.go
+++ roster.go
@@ -4,13 +4,23 @@ import (
 	
 )
 
-func addWrestler(wrestler Wrestler) Wrestler{
-	
+func addWrestler(wrestler Wrestler, ringname, alignment, signature_move string) Wrestler{
+	wrestler.ringname = ringname
+	wrestler.alignment = alignment
+	wrestler.signature_move = signature_move
+
 	return wrestler
 }
 
-func listWrestler(wrestler Wrestler) Wrestler{
-	return wrestler
+func listWrestler(ringname string) map[int]Wrestler{
+	//Some get statement here
+	//Map to Wrestler
+
+	wrestlers := map[int]Wrestler{
+
+	}
+
+	return wrestlers
 }
 
 func removeWrestler(wrestler Wrestler){
